f148d4f4beb8c5417bbd9d95a5ff47741f33ec23
calebanthony <caleb.m.anthony@gmail.com>
2017-08-16 12:54:00 -0600

    Update for Ghost 1.0

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,12 @@
 // Your JS can go here! Pull in or write whatever you want.
 //
-// For example, to add in Bootstrap 4 JS (I know, this sucks):
-// jQuery is already being pulled in via the Webpack file.
-//
-import '../../node_modules/bootstrap/js/dist/button.js';
-//
+// For example, to add in Bootstrap 4 JS:
+// jQuery is already being pulled in via the webpack.config.js file.
+
+require('bootstrap');
+
 // It's recommended that you only pull in the Bootstrap modules you need.
+// You can do this by pulling in the /js/dist/*.js files under bootstrap
+// Example:
+//
+// require('bootstrap/js/dist/alert.js');
